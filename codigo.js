3b7d63eb3dedb29b2a63e47508f8604633a341a8
Claudio-1201 <74226746@continental.edu.pe>
2026-05-13 14:10:05 -0500

    PARA MAYLI

--- codigo.js
+++ codigo.js
@@ -1,3 +1,13 @@
-function mostrarMensaje(mensaje) {
-    alert(mensaje);
+const contenedor = document.querySelector(".contenedor");
+
+function irDerecha(){
+    contenedor.style.transform = "translateX(-100vw)";
+}
+
+function irIzquierda(){
+    contenedor.style.transform = "translateX(0)";
+}
+
+function mostrarMensaje(){
+    alert("💖 Gracias por existir MAYLI, TE AMO MUCHOOOOOOOOOOOOOOOO 💖");
 }
